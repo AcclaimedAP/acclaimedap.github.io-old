5ccb40e0653474c8abbc11e1c87a5fbfe92caa11
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2023-02-11 22:13:50 +0100

    fix suffix added to url?

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/cv2023/',
+  base: '/',
   css: {
     preprocessorOptions: {
       scss: {
